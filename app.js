e01e59eb47734fb63ae54d0e274046f253ea5611
Giuliano <giulianofurgol@hotmail.com>
2018-02-05 18:00:33 +0100

    BDD connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var db = require('./db/db');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var admin = require('./routes/admin');
