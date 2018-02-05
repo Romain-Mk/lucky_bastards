42b151df58d2ed5375262ea236571745add0510c
Romain Marquet <romain.marquet@hotmail.fr>
2018-02-05 16:32:48 +0100

    creation routes

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('admin liste user stories');
+});
+
+router.get('/account', function(req, res, next) {
+  res.send('user info');
+});
+
+router.get('/stories/:id', function(req, res, next) {
+  res.send('fiche story ' + req.params.id);
+});
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.send('accueil sign up');
+});
+
+router.get('/login', function(req, res, next) {
+  res.send('accueil login');
+});
+
+router.get('/stories/:id', function(req, res, next) {
+  res.send('accueil stories ' + req.params.id);
+});
+
+router.get('/author/:id', function(req, res, next) {
+  res.send('accueil ta race ' + req.params.id);
 });
 
 module.exports = router;
