a31482ff80a8ab649d641c52124f3c75e182d814
giuliano2014 <giulianofurgol@hotmail.com>
2018-02-08 14:37:13 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -7,16 +7,7 @@ router.all('/*', function (req, res, next) {
     res.redirect('/login');
   }
   next(); // pass control to the next handler
-})
-
-// On est passé par là mais Super Rital a trouvé la solution au-dessus !!
-// router.get('/', function(req, res, next) {
-//   if (!req.session.userId) {
-//     res.redirect('/login');
-//   } else {
-//     res.render('admin/index');
-//   }
-// });
+});
 
 router.get('/', function(req, res, next) {
   res.render('admin/index');
