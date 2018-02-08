ead4e0778093d5836628475390bbd71171359ac0
Romain Marquet <romain.marquet@hotmail.fr>
2018-02-08 18:22:20 +0100

    fiche_story_admin

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,6 @@ var storyModel = require('../models/story');
 router.get('/', function(req, res, next) {
   // find de la story storyList
   storyModel.find({}, function(err, stories){
-    console.log("les stories sont: ", stories);
     res.render('admin/index', {storyList: stories});
   })
 });
@@ -15,56 +14,34 @@ router.get('/account', function(req, res, next) {
   res.render('admin/account');
 });
 
-router.get('/mystories/:id', function(req, res, next) {
-  res.render('admin/story');
+router.post('/mystories/:id', function(req, res, next) {
+  console.log("ok route mystories/:id");
+  var newStory = new storyModel ({
+    tag: null,
+    category: null,
+    title: req.body.title,
+    text: req.body.text_zone,
+    img: null,
+    lang: null,
+    place: null,
+    authorId: null,
+    publish: null
+  });
+  newStory.save(
+    function(err, stories) {
+      storyModel.find(
+        {title: req.body.title, text: req.body.text_zone},
+          function (error, storyList) {
+            res.render('admin/story', {storyList, title: req.body.title, text: req.body.text_zone});
+          }
+      )
+    }
+  )
 });
 
-router.route('/newstory')
-
-  .get(function(req, res, next) {
-    res.render('admin/newstory');
-  })
-
-  .post(function(req, res, next) {
-     var newStory = new storyModel ({
-       title: req.body.title,
-       text: req.body.text_zone,
-       img: null,
-       tag: null,
-       category: null,
-       lang: null,
-       place: null,
-       authorId: null,
-       publish: null
-      });
-    console.log(newStory);
-
-    newStory.save(function (error, story) {
-      if (error) throw (error);
-      console.log(story);
-      res.render('admin/index')
-      });
-
-  });
-
-
-
-// router.get('/newstory', function(req, res, next) {
-//   res.render('admin/newstory');
-// });
-//
-// router.post('/newstory', function(req, res, next) {
-//   console.log(req.body);
-// });
-
- //  .post(function(req, res, next) {
-
-
-
- //       res.redirect('/URL SUR LAQUELLE ON REVIENT');
- //     });
- //
- // });
+router.get('/newstory', function(req, res, next) {
+  res.render('admin/newstory');
+});
 
 router.get('/mystories/:id/edit', function(req, res, next) {
  res.render('admin/newstory');
