c6e9672e00d3cbed19f3c92143b46105d48e95ac
flora <flora.marteau@live.fr>
2018-02-09 13:11:47 +0100

    wip

--- routes/admin.js
+++ routes/admin.js
@@ -25,6 +25,34 @@ router.get('/account', function(req, res, next) {
   res.render('admin/account', {log});
 });
 
+router.post('/account', function(req, res) {
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+   // var profilepic = req.files.profilepic;
+   // Pour l'instant on nomme toutes les images enregistrées img.jpg, après on pourra utiliser req.session.user._id
+   // pour pouvoir renommer l'image en fonction de l'user qui l'a uploadée
+   var fileName = 'img';
+   // Use the mv() method to place the file somewhere on your server
+   req.files.img.mv('./public' + '/images/profilepics/' + fileName + '.jpg' , function(err) {
+     if(err){
+    console.log(err);
+     }else{
+    res.render('admin/account');
+    console.log("uploaded");
+    }
+   });
+ });
+
+// router.post ('/account', function(req, res) {
+//   var facebook = req.body.facebook;
+//   var twitter = req.body.twitter;
+//   var instagram = req.body.instagram;
+//   var website = req.body.website;
+//
+//   res.send(instagram);
+//   console.log(instagram);
+//
+// })
+
 router.get('/stories/:id', function(req, res, next) {
   res.render('admin/story', {log});
 });
