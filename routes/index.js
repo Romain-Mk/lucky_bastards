d842660554a7ce1eff1adf8c801a3b0506dbbfa7
giuliano2014 <giulianofurgol@hotmail.com>
2018-02-12 10:07:08 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -20,20 +20,20 @@ router.all('/*', function (req, res, next) {
 
 router.get('/', function(req, res, next) {
   Story.find({}, function(err, stories) {
-    res.render('index', {stories}, {log});
+    res.render('index', {stories, log});
   });
 });
 
 //req.params.id = trouve moi l'élément dont l'id de la BDD correspond à celui qu'on te passe dans l'url
 router.get('/stories/:id', function(req, res, next) {
   Story.findOne({_id: req.params.id}, function (error, story) {
-    res.render('story', {story}, {log});
+    res.render('story', {story, log});
   });
 });
 
 router.get('/authors/:id', function(req, res, next) {
   Story.find({title: req.body.title, text: req.body.text_zone}, function (error, stories) {
-        res.render('author', {stories, title: req.body.title, text: req.body.text_zone}, {log});
+        res.render('author', {stories, title: req.body.title, text: req.body.text_zone, log});
       });
 });
 
