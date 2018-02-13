54f2820a035cc61c4b284f73acac493bc367dec4
Christophe Garcia <christophegarcia01@gmail.com>
2018-02-13 11:54:50 +0100

    redirect login

--- routes/login.js
+++ routes/login.js
@@ -4,44 +4,62 @@ var bcrypt = require('bcrypt');
 
 var User = require('../models/user');
 
+var from = null;
+
 router.route('/')
-
   .get(function(req, res, next) {
-    res.render('login');
+    res.render('login', {from});
   })
-
   .post(function(req, res, next) {
+    login(req, res, next);
+  });
 
-    var email = req.body.email;
-    var password = req.body.password;
+router.route('/:from')
+  .get(function(req, res, next) {
+    res.render('login', {from: req.params.from});
+  })
+  .post(function(req, res, next) {
+    login(req, res, next);
+  });
 
-    User.find({ email: email }, function(err, user) {
-      if (err) throw err;
+function login(req, res, next) {
 
-      // Check if user is defined and has at least one element
-      if (typeof user !== 'undefined' && user.length > 0) {
+  var email = req.body.email;
+  var password = req.body.password;
 
-        bcrypt.compare(password, user[0].password, function(err, match) {
+  User.find({ email: email }, function(err, user) {
+    if (err) throw err;
 
-          if (err) throw(err);
+    // Check if user is defined and has at least one element
+    if (typeof user !== 'undefined' && user.length > 0) {
 
-          if (match === true) {
-            // password matches
-            req.session.userId = user[0]._id;
+      bcrypt.compare(password, user[0].password, function(err, match) {
+
+        if (err) throw(err);
+
+        if (match === true) {
+          // password matches
+          req.session.userId = user[0]._id;
+
+          if (req.params.from === 'addbutton') {
+            res.redirect('/admin/newstory');
+          } else {
             res.redirect('/');
-          } else if (match === false) {
-            // password does not match
-            res.render('login');
           }
 
-        });
+        } else if (match === false) {
+          // password does not match
+          res.render('login', {from: req.params.from});
+        }
 
-      } else {
-        res.render('login');
-      }
+      });
 
-    });
+    } else {
+      res.render('login', {from});
+    }
 
   });
+
+}
 
 module.exports = router;
