ce83a676c88a35562d554a90d3d613a9c31a909f
Giuliano <giulianofurgol@hotmail.com>
2018-02-14 17:43:08 +0100

    Fixed last bugs

--- routes/index.js
+++ routes/index.js
@@ -39,8 +39,7 @@ router.get('/stories/:id', function(req, res, next) {
 router.get('/authors/:id', function(req, res, next) {
   var id = req.params.id;
   User.findOne({_id: id}, function(error, user) {
-    if (error) throw error;
-    Story.find({authorId: id}, function(error, story) {
+    Story.find({authorId: id}).sort({createdAt: -1}).exec(function(error, story) {
       if (error) throw error;
       res.render('author', {user, story, log});
     });
